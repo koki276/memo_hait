dabcf0b08ec2f436571928044fc4364ecb878c2d
LAPTOP-16ET0KVR\inoue koki <seaton1605.211@softbank.ne.jp>
2018-10-09 09:09:27 +0900

    init

---
+++ memo/settings/heroku.py
@@ -0,0 +1,14 @@
+from .common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+    }
+}
